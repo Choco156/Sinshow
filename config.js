c4d1725a56422c9098c726e9189e8cfedd5ccc37
Choco156 <gm7894@gmail.com>
2026-09-16 19:19:49 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+/* Supabase 접속 정보.
+   키가 바뀌면 이 파일만 고치면 모든 도구에 반영됩니다.
+   여기 있는 키는 공개용(anon)입니다. service_role / sb_secret 키는 절대 넣지 마세요. */
+
+var SUPABASE_URL = "https://qvmnbdimdokxbpzjxiyp.supabase.co";
+var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bW5iZGltZG9reGJwemp4aXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MjU5MjMsImV4cCI6MjEwNTEwMTkyM30.YZ1qDEQ9adO1iJxb-906mLNEIrH-0VR8YEu-4-fSZdY";
